599d2a77795d53159fc3d40cc81ba22b95e1b264
Fabio Ono Tavoloni <fabioono25@gmail.com>
2020-03-08 15:57:45 +0000

    using cargo

---
+++ hello_cargo/target/debug/deps/hello_cargo-042c604b75086dae.d
@@ -0,0 +1,5 @@
+/home/ono/study/rust_tests/hello_cargo/target/debug/deps/hello_cargo-042c604b75086dae: src/main.rs
+
+/home/ono/study/rust_tests/hello_cargo/target/debug/deps/hello_cargo-042c604b75086dae.d: src/main.rs
+
+src/main.rs:
---
+++ hello_cargo/target/debug/hello_cargo.d
@@ -0,0 +1,1 @@
+/home/ono/study/rust_tests/hello_cargo/target/debug/hello_cargo: /home/ono/study/rust_tests/hello_cargo/src/main.rs
