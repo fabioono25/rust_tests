da8b8000db29e32f6f0c72b4c89468b59a1eb07a
Fabio Ono Tavoloni <fabioono25@gmail.com>
2020-03-08 18:29:22 +0000

    slices

---
+++ slices/target/debug/deps/slices-dc37ddcae0962265.d
@@ -0,0 +1,5 @@
+/home/ono/study/rust_tests/slices/target/debug/deps/slices-dc37ddcae0962265: src/main.rs
+
+/home/ono/study/rust_tests/slices/target/debug/deps/slices-dc37ddcae0962265.d: src/main.rs
+
+src/main.rs:
---
+++ slices/target/debug/slices.d
@@ -0,0 +1,1 @@
+/home/ono/study/rust_tests/slices/target/debug/slices: /home/ono/study/rust_tests/slices/src/main.rs
