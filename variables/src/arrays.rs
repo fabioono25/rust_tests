28c6a1e3303c2e6396a690e1383ca5a5486a0014
Fabio Ono Tavoloni <fabioono25@gmail.com>
2020-03-08 16:44:25 +0000

    Arrays

---
+++ variables/src/arrays.rs
@@ -0,0 +1,8 @@
+fn main(){
+    let months = ["January", "February", "March", "April", "May", "June", "July",
+        "August", "September", "October", "November", "December"];
+
+    println!("First month = {}", months[0]);
+
+    println!("Forteenth month = {}", months[13]);
+}
