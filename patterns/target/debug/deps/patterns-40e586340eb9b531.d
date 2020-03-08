58bb97c0253636e4191284604b7fca4217c60954
Fabio Ono Tavoloni <fabioono25@gmail.com>
2020-03-08 19:26:04 +0000

    pattern matching

---
+++ patterns/target/debug/deps/patterns-40e586340eb9b531.d
@@ -0,0 +1,5 @@
+/home/ono/study/rust_tests/patterns/target/debug/deps/patterns-40e586340eb9b531: src/main.rs
+
+/home/ono/study/rust_tests/patterns/target/debug/deps/patterns-40e586340eb9b531.d: src/main.rs
+
+src/main.rs:
---
+++ patterns/target/debug/patterns.d
@@ -0,0 +1,1 @@
+/home/ono/study/rust_tests/patterns/target/debug/patterns: /home/ono/study/rust_tests/patterns/src/main.rs
