fe79851d017abaa11a35f34554315179d073b760
Fabio Ono Tavoloni <fabioono25@gmail.com>
2020-03-08 16:08:10 +0000

    variables

---
+++ variables/target/debug/deps/variables-d10ee2f2200a7d59.d
@@ -0,0 +1,5 @@
+/home/ono/study/rust_tests/variables/target/debug/deps/variables-d10ee2f2200a7d59: src/main.rs
+
+/home/ono/study/rust_tests/variables/target/debug/deps/variables-d10ee2f2200a7d59.d: src/main.rs
+
+src/main.rs:
---
+++ variables/target/debug/variables.d
@@ -0,0 +1,1 @@
+/home/ono/study/rust_tests/variables/target/debug/variables: /home/ono/study/rust_tests/variables/src/main.rs
