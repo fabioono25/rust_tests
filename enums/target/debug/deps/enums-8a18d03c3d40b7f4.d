8e865ec06d464a6c457e4488f8b3bcc44d8091e0
Fabio Ono Tavoloni <fabioono25@gmail.com>
2020-03-08 19:16:50 +0000

    enums

---
+++ enums/target/debug/deps/enums-8a18d03c3d40b7f4.d
@@ -0,0 +1,5 @@
+/home/ono/study/rust_tests/enums/target/debug/deps/enums-8a18d03c3d40b7f4: src/main.rs
+
+/home/ono/study/rust_tests/enums/target/debug/deps/enums-8a18d03c3d40b7f4.d: src/main.rs
+
+src/main.rs:
---
+++ enums/target/debug/enums.d
@@ -0,0 +1,1 @@
+/home/ono/study/rust_tests/enums/target/debug/enums: /home/ono/study/rust_tests/enums/src/main.rs
